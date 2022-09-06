b321261fb54187d5ff1c7e70d0008d47f36a9eec
phitheone <102269729+phitheone@users.noreply.github.com>
2022-09-06 08:59:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import smokeImg from "./Images/greensmoke.png";
+import logo from "./Images/logo.png";
+import motto from "./Images/motto.png";
+import sign from "./Images/bt2.png";
+import discord from "./Images/bt4.png";
+import twitter from "./Images/bt5.png";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <div className="LoadingScreen">
+          <img className="Logo" src={logo} />
+          <img className="Motto" src={motto} />
+        </div>
+      ) : (
+        <div className="App">
+          <div className="Sign">
+            <img src={sign} alt="smoke" />
+          </div>
+          <div className="WC">
+            <a href="https://www.launchmynft.io/" target="_blank">
+              <div className="Mint">
+                <p>&nbsp;</p>
+              </div>
+            </a>
+            <div className="smoke-wrap">
+              <img className="smoke" src={smokeImg} alt="smoke" />
+            </div>
+            <div className="smoke-wrap">
+              <img className="smoke2" src={smokeImg} alt="smoke" />
+            </div>
+            <div className="smoke-wrap">
+              <img className="smoke3" src={smokeImg} alt="smoke" />
+            </div>
+          </div>
+          <div className="Socials">
+            <a href="https://twitter.com/p00ps_NFT" target="_blank">
+              <img
+                className="imgT"
+                href="https://twitter.com/p00ps_NFT"
+                src={twitter}
+                alt="twitter"
+              />
+            </a>
+            <a href="https://discord.gg/svbJCVPU" target="_blank">
+              <img className="imgD" src={discord} alt="discord" />
+            </a>
+          </div>
+          <div className="Footer">
+            <p>Copyright © 2022. Made with 💩 by the p00ptopia dev team!</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
